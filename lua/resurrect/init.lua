576b22d3f7c40bf0cfc63355ad8e02278493827d
Taylor Bartlett <taybart@gmail.com>
2023-11-29 14:52:26 -0700

    refact: rename functions

--- lua/resurrect/init.lua
+++ lua/resurrect/init.lua
@@ -24,10 +24,6 @@ end
 local function enable()
   local group_name = 'resurrect'
   vim.api.nvim_create_augroup(group_name, {})
-  -- vim.api.nvim_create_autocmd('UIEnter', {
-  --   group = group_name,
-  --   callback = load_buffers,
-  -- })
   vim.api.nvim_create_autocmd('BufAdd', {
     group = group_name,
     callback = add,
@@ -38,7 +34,7 @@ local function enable()
   })
 end
 
-local function restore()
+local function resurrect()
   enable()
   local bfs = file.load_buffers()
   if bfs == nil then
@@ -51,7 +47,7 @@ local function restore()
   end
 end
 
-local function start_session()
+local function start()
   local bufnums = vim.tbl_filter(function(buf)
     return vim.api.nvim_buf_is_valid(buf) and vim.api.nvim_buf_get_option(buf, 'buflisted')
   end, vim.api.nvim_list_bufs())
@@ -68,8 +64,8 @@ function M.setup()
   if vim.fn.filereadable('.resurrect') == 1 then
     enable()
   end
-  vim.api.nvim_create_user_command('Resurrect', restore, {})
-  vim.api.nvim_create_user_command('ResurrectStart', start_session, {})
+  vim.api.nvim_create_user_command('Resurrect', resurrect, {})
+  vim.api.nvim_create_user_command('ResurrectStart', start, {})
 end
 
 return M
