f710ecc368d22ad0637069f7ed9b91665c6c3d17
Taylor Bartlett <taybart@gmail.com>
2025-03-18 09:41:37 -0600

    fix: session id issue

--- lua/resurrect/db.lua
+++ lua/resurrect/db.lua
@@ -40,17 +40,17 @@ function M:new_session(in_name)
   local sessions = self.db:select('sessions', { where = { name = session_name } })
   if #sessions > 0 then
     vim.print('found existing session')
-    M.session.id = sessions[1].id
-    M.session.name = sessions[1].name
-    M.session.files = M:get_files(M.session)
+    self.session.id = sessions[1].id
+    self.session.name = sessions[1].name
+    self.session.files = M:get_files(self.session)
     return false
   end
   local success, id = self.db:insert('sessions', { name = session_name })
   if not success then
     error('could not add new session')
   end
-  M.session.id = id
-  M.session.name = session_name
+  self.session.id = id
+  self.session.name = session_name
   return true
 end
 
@@ -58,7 +58,7 @@ function M:has_sessions()
   local session_name = '%:' .. vim.fn.getcwd()
   local sessions = self.db:eval('select count(*) from sessions where name like ?', session_name)
   local count = sessions[1]['count(*)']
-  M.session_count = count
+  self.session_count = count
   return count > 0
 end
 
@@ -71,7 +71,7 @@ function M:get_session(arg)
         s.files = M:get_files(s)
       end
       u.choose_session(
-        { title = 'Found sessions', preview_depth = M.config.preview_depth },
+        { title = 'Found sessions', preview_depth = self.config.preview_depth },
         sessions,
         function(s)
           arg(u.session_shortname(s.name), s)
@@ -88,8 +88,8 @@ end
 function M:load_session(cb)
   local session_name = '%:' .. vim.fn.getcwd()
   local sessions = self.db:eval('select * from sessions where name like ?', session_name)
-  if #sessions == 1 and not M.config.always_choose then
-    M.id = sessions[1].id
+  if #sessions == 1 and not self.config.always_choose then
+    self.session.id = sessions[1].id
     cb(u.session_shortname(sessions[1].name), M:load())
     return
   end
@@ -98,10 +98,10 @@ function M:load_session(cb)
       s.files = M:get_files(s)
     end
     u.choose_session(
-      { title = 'Found sessions', preview_depth = M.config.preview_depth },
+      { title = 'Found sessions', preview_depth = self.config.preview_depth },
       sessions,
       function(s)
-        M.id = s.id
+        self.session.id = s.id
         cb(u.session_shortname(s.name), M:load())
       end
     )
@@ -115,8 +115,8 @@ function M:delete_session(id)
 end
 
 function M:load()
-  self.session.files = self.db:select('files', { where = { session_id = self.id } })
-  if M.config.debug then
+  self.session.files = self.db:select('files', { where = { session_id = self.session.id } })
+  if self.config.debug then
     vim.print(self.session.files)
   end
   return self.session.files
@@ -129,11 +129,12 @@ function M:get_files(session)
 end
 
 function M:add_file(filepath) -- TODO add cursor position
-  self.db:insert('files', { path = filepath, session_id = M.session.id })
+  vim.print('session', self.session)
+  self.db:insert('files', { path = filepath, session_id = self.session.id })
 end
 
 function M:del_file(filepath)
-  self.db:delete('files', { path = filepath, session_id = M.session.id })
+  self.db:delete('files', { where = { path = filepath, session_id = self.session.id } })
 end
 
 return M
